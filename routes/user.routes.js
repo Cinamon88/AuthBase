5c8127c0da26af1bfeaa1375125619c1f02c2525
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-03 13:41:22 +0100

    add profile & profile settings

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/logged', (req, res) => {
-  res.render('logged');
+const loggedIn = (req, res, next) => {
+    if(!req.isAuthenticated()) {
+        return res.redirect('user/no-permission');
+    } else {
+        next();
+    }
+};
+
+router.get('/logged', loggedIn, (req, res) => {
+  res.render('logged', {
+  });
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
+});
+
+router.get('/profile', loggedIn, (req, res) => {
+    res.render('profile');
+});
+
+router.get('/profile/settings', loggedIn, (req, res) => {
+    res.render('profileSettings');
 });
 
 
