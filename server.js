95f68348ecb70c61c095723c4be1d0336c5603ac
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-03 10:28:20 +0100

    check if log in successful

--- server.js
+++ server.js
@@ -45,9 +45,11 @@ app.get('/', (req, res) => {
 app.get('/auth/google',
   passport.authenticate('google', { scope: ['email', 'profile'] }));
 
-app.get('/auth/google/callback', (req, res) => {
-  res.send(`I'm back from Google!`);
-});
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
+  (req, res) => {
+    res.redirect('/user/logged');
+  }
+);
 
 app.get('/user/logged', (req, res) => {
   res.render('logged');
